1e68749cf3bd5c03ae5ac9a55e6cb3cd6c79d081
Baptiste <baptiste.viry@orange.fr>
2019-04-11 17:55:56 -0500

    adding tagCloud bono

--- webAppProject/taller2/urls.py
+++ webAppProject/taller2/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('presentacionDB/', views.presentacionDB, name='presentacionDB'),
     path('charts/', views.charts, name='charts'),
+    path('hashtagWordCloud/', views.hashtagWordCloud, name='hashtagWordCloud'),
 ]
--- webAppProject/taller2/views.py
+++ webAppProject/taller2/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 from pymongo import MongoClient
 
+import json, random
+
 client = MongoClient('bigdata-mongodb-04.virtual.uniandes.edu.co', 8087)
 db = client.Grupo10
 collectionVenezuela = db.Venezuela
@@ -20,10 +22,26 @@ def presentacionDB(request):
     countCollections = { 'countVenezuela':countVenezuela, 'countMinga':countMinga, 'countJep':countJep}
     context = { 'countCollections':countCollections }
 
-    
     return render(request, "taller2/presentacionDB.html", context)
 
 def charts(request):
     dataPolarity = { 'positive':120000, 'neutral':150000, 'negative':90000, 'insult':30000 }
     context = {'dataPolarity': dataPolarity}
     return render(request, "taller2/charts.html", context)
+
+def hashtagWordCloud(request):
+    #groupBy hashtags
+    i = 10
+    hashtagsArray = []
+    hashtagsCursor = collectionVenezuela.aggregate([{'$unwind': '$entities.hashtags'}, { '$group': { '_id': '$entities.hashtags.text', 'count': {'$sum': 1}}}, { '$sort': { 'count': -1 }},{ '$limit': 10 }])
+    for h in hashtagsCursor:
+        x = {"hashtag": h["_id"], "count": h["count"], "rank": i}
+        hashtagsArray.append(x)
+        i -= 1 
+
+    random.shuffle(hashtagsArray)
+    print(hashtagsArray)
+
+
+    context = { "hashtags": hashtagsArray}
+    return render(request, "taller2/hashtagWordCloud.html", context)
